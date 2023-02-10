c99b92a5583aa6cdae2c94441131d5dee97fe29d
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-10 20:05:52 -0300

    :sparkles: feat: Criação de Interface de cursos para metodos no repositorio

---
+++ Orange/Repository/Interfaces/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Orange.Model;
+
+namespace Orange.Repository.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<List<Course>> AllCourses();
+        Task<Course> GetCourseById(int id);
+        Task<Course> AddCourse(Course Course);
+        Task<Course> UpdateCourse(int id, Course course);
+        Task<bool> DeleteCourse(int id);
+    }
+}
