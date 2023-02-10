38fc9f787b4dfa849dbf2c2a26b96a99e5b39f5d
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-10 12:12:58 -0300

    :recycle: Adição do mapeamento de cursos no contexto da aplicação

--- Orange/Data/OrangeContext.cs
+++ Orange/Data/OrangeContext.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Orange.Mappings;
+using Orange.Model;
 
 namespace Orange.Data
 {
     public class OrangeContext : DbContext
     {
-
-
         public OrangeContext(DbContextOptions<OrangeContext> options) : base(options)
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TrailMapping());
+            modelBuilder.ApplyConfiguration(new CourseMapping());
+        }
 
+        public DbSet<Trail> Trail { get; set; }
+        public DbSet<Course> Course { get; set; }
 
     }
 }
