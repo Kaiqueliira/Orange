e4d99493eb280058bae68ea3c8c5e2ec110ed2f7
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-10 10:10:48 -0300

    :sparkles: Adicionando Mapeamento do Modelo Trail para o Entity

---
+++ Orange/Mappings/TrailMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Orange.Model;
+
+namespace Orange.Mappings
+{
+    public class TrailMapping : IEntityTypeConfiguration<Trail>
+    {
+        public void Configure(EntityTypeBuilder<Trail> builder)
+        {
+            builder.ToTable("Trail");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(p => p.Title)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasColumnType("varchar(1024)");
+
+        }
+    }
+}
