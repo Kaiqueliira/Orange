7ba9d24138f88e03dc8f57bafc900689141cd370
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-10 09:59:43 -0300

    :sparkles: Criação de modelo de trilhas

---
+++ Orange/Model/Trail.cs
@@ -0,0 +1,10 @@
+﻿namespace Orange.Model
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
