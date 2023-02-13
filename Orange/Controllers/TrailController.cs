eb789f6ae771c36687241e2448611c35013f37bf
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-13 15:03:29 -0300

    :sparkles: feat: adicionando comentarios para documentação do swagger

--- Orange/Controllers/TrailController.cs
+++ Orange/Controllers/TrailController.cs
@@ -17,12 +17,23 @@ namespace Orange.Controllers
             _trailRepository = trailRepository;
         }
 
+        /// <summary>
+        /// Pega Todas as Trilhas cadastradas no banco de dados
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpGet]
         public async Task<ActionResult<List<Trail>>> GetAll()
         {
             List<Trail> trails = await _trailRepository.GetAllTrails();
             return Ok(trails);
         }
+        /// <summary>
+        /// Pega uma Trilha a partir do ID no Banco de dados
+        /// </summary>
+        /// <param name="id">campo necessário para pegar uma trilha</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Trail>> GetById(int id)
         {
@@ -30,13 +41,25 @@ namespace Orange.Controllers
             return Ok(trail);
 
         }
+        /// <summary>
+        /// Adiciona uma Trilha ao banco de dados
+        /// </summary>
+        /// <param name="trailModel">Objeto com os campos necessários para criação de uma Trilha</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpPost]
         public async Task<ActionResult<Trail>> Add([FromBody] Trail trailModel)
         {
             Trail trail = await _trailRepository.AddTrail(trailModel);
             return Ok(trail);
         }
-
+        /// <summary>
+        /// Atualiza uma Trilha a partir do ID e passando o objeto no body para atualizar no Banco de dados
+        /// </summary>
+        /// <param name="id">campo necessário para Atualizar uma trilha</param>
+        ///  <param name="trailModel">Objeto com os campos necessários para Atualizar uma trilha</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpPut("{id:int}")]
         public async Task<ActionResult<Trail>> Update(int id, [FromBody] Trail trailModel)
         {
@@ -44,6 +67,12 @@ namespace Orange.Controllers
             Trail trail = await _trailRepository.UpdateTrail(id, trailModel);
             return Ok(trail);
         }
+        /// <summary>
+        /// Deleta uma Trilha a partir do ID no Banco de dados
+        /// </summary>
+        /// <param name="id">campo necessários para Deletar uma trilha</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpDelete("{id:int}")]
         public async Task<ActionResult<Trail>> Remove(int id)
         {
